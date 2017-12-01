91eccff6d847589163d74e255b13b6952b493824
Alistair Chapman <alistair@agchapman.com>
2017-12-01 11:49:01 +1000

    Add UPower code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Tmds.DBus;
+using UPower.DBus;
 
 namespace dbus
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting D-Bus service");
+            var system = Connection.System;
+            var upower = system.CreateProxy<IUPower>("org.freedesktop.UPower", "/org/freedesktop/UPower");
+            var props = await upower.GetAllAsync();
+            Console.WriteLine($"You are on {(props.OnBattery ? "battery" : "AC power")}");
         }
     }
 }
